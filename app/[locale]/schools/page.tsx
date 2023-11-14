93271756f6f9eea62ae99309b0a9cb57866a4adf
Julian Vogel <birdman.coding.1991@gmail.com>
2023-11-14 14:32:43 +0100

    maps auto complete

--- app/[locale]/schools/page.tsx
+++ app/[locale]/schools/page.tsx
@@ -1,8 +1,100 @@
-import React from "react";
-import axios from "axios";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { set, useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useState } from "react";
+
+const languages = ["en", "fr", "de", "es", "pt", "ru"] as const;
+
+const FormSchema = z.object({
+  language: z.string({
+    required_error: "Please select a language.",
+  }),
+});
 
 function SchoolsPage() {
-  return <div>hai schools</div>;
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
+  const [inputValue, setInputValue] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="language"
+          render={({ field }) => {
+            return (
+              <FormItem className="flex flex-col">
+                <FormLabel>Language</FormLabel>
+
+                <Command>
+                  <CommandInput
+                    placeholder="Search language..."
+                    onValueChange={(val) => {
+                      setInputValue(val), form.setValue("language", "");
+                      setIsOpen(true);
+                    }}
+                    value={inputValue}
+                  />
+                  {isOpen && <CommandEmpty>No language found.</CommandEmpty>}
+                  <CommandGroup>
+                    {isOpen &&
+                      languages.map((language, idx) => (
+                        <CommandItem
+                          value={language}
+                          key={idx}
+                          onSelect={() => {
+                            form.setValue("language", language);
+                            setInputValue(language);
+                            setIsOpen(false);
+                          }}
+                        >
+                          {language}
+                        </CommandItem>
+                      ))}
+                  </CommandGroup>
+                </Command>
+
+                <FormDescription>
+                  This is the language that will be used in the dashboard.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
 
 export default SchoolsPage;
--- components/school/AddressInput.tsx
+++ components/school/AddressInput.tsx
@@ -2,13 +2,20 @@
 import React, { useEffect, useRef, useState } from "react";
 import {
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "../ui/form";
-import { Input } from "../ui/input";
-import { UseFormReturn, set } from "react-hook-form";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import { UseFormReturn } from "react-hook-form";
 import { CreateEmployeeType } from "@/types/employeeSchema";
 import getGoogleMapsApiClient from "@/lib/googleApiClient";
 
@@ -17,79 +24,130 @@ type Props = {
 };
 
 export default function AddressInput({ form }: Props) {
-  const [formattedAddress, setFormattedAddress] = useState("");
-  const addressField = form.watch("address");
+  const [formattedAddress, setFormattedAddress] = useState<string[]>([]);
   const sessionTokenRef = useRef<string>();
+  const timeoutRef = useRef<NodeJS.Timeout>();
 
   async function onAddressChange(address: string) {
-    try {
-      const google = await getGoogleMapsApiClient();
-      if (!sessionTokenRef.current) {
-        sessionTokenRef.current =
-          new google.maps.places.AutocompleteSessionToken() as string;
-      }
-      const { predictions } =
-        await new google.maps.places.AutocompleteService().getPlacePredictions({
-          input: address,
-          types: ["geocode"],
-          sessionToken: sessionTokenRef.current,
-          region: "eur",
-          //componentRestrictions: { country: "de" },
-        });
-
-      const service = new google.maps.places.PlacesService(
-        document.createElement("div")
-      );
-      service.getDetails(
-        { placeId: predictions[0].place_id },
-        (place, status) => {
-          if (
-            status === google.maps.places.PlacesServiceStatus.OK &&
-            place &&
-            place.address_components
-          ) {
-            // Überprüfen, ob die Adresse eine Straße (route) und eine Hausnummer (street_number) enthält
-            const hasStreet = place.address_components.some((component) =>
-              component.types.includes("route")
-            );
-            const hasNumber = place.address_components.some((component) =>
-              component.types.includes("street_number")
-            );
-
-            if (hasStreet && hasNumber && place.formatted_address) {
-              // Adresse mit Straße und Hausnummer gefunden
-              console.log(place);
-              setFormattedAddress(place.formatted_address);
-            } else {
-              setFormattedAddress("");
-            }
-          }
-        }
-      );
-    } catch (err) {
-      setFormattedAddress("");
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
     }
+
+    timeoutRef.current = setTimeout(async () => {
+      try {
+        const google = await getGoogleMapsApiClient();
+        if (!sessionTokenRef.current) {
+          sessionTokenRef.current =
+            new google.maps.places.AutocompleteSessionToken() as string;
+        }
+        const { predictions } =
+          await new google.maps.places.AutocompleteService().getPlacePredictions(
+            {
+              input: address,
+              types: ["geocode"],
+              sessionToken: sessionTokenRef.current,
+              region: "eur",
+              //componentRestrictions: { country: "de" },
+            }
+          );
+
+        const service = new google.maps.places.PlacesService(
+          document.createElement("div")
+        );
+
+        predictions.forEach((predictedAddress) => {
+          service.getDetails(
+            { placeId: predictedAddress.place_id },
+            (place, status) => {
+              if (
+                status === google.maps.places.PlacesServiceStatus.OK &&
+                place &&
+                place.address_components
+              ) {
+                // Überprüfen, ob die Adresse eine Straße (route) und eine Hausnummer (street_number) enthält
+                const hasStreet = place.address_components.some((component) =>
+                  component.types.includes("route")
+                );
+                const hasNumber = place.address_components.some((component) =>
+                  component.types.includes("street_number")
+                );
+
+                if (hasStreet && hasNumber && place.formatted_address) {
+                  // Adresse mit Straße und Hausnummer gefunden
+
+                  if (!formattedAddress.includes(place.formatted_address)) {
+                    // Adresse mit Straße und Hausnummer gefunden und noch nicht in der Liste
+                    setFormattedAddress((prev) => [
+                      ...prev,
+                      place.formatted_address as string,
+                    ]);
+                  }
+                } else {
+                  setFormattedAddress([]);
+                }
+              }
+            }
+          );
+        });
+      } catch (err) {
+        setFormattedAddress([]);
+      }
+    }, 150);
   }
 
-  useEffect(() => {
-    if (addressField) {
-      onAddressChange(addressField);
-    }
-  }, [addressField, onAddressChange]);
+  const [inputValue, setInputValue] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <FormField
       control={form.control}
       name="address"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>Address</FormLabel>
-          <FormControl>
-            <Input placeholder="Unicorn Road" {...field} />
-          </FormControl>
-          {formattedAddress && <FormMessage>{formattedAddress}</FormMessage>}
-        </FormItem>
-      )}
+      render={({ field }) => {
+        return (
+          <FormItem className="flex flex-col">
+            <FormLabel>Address</FormLabel>
+
+            <Command>
+              <CommandInput
+                placeholder="Search language..."
+                onValueChange={(val) => {
+                  setInputValue(val);
+                  form.setValue("address", "");
+                  setIsOpen(true);
+                  onAddressChange(val);
+                }}
+                value={inputValue}
+              />
+              {isOpen && formattedAddress.length > 0 && (
+                <CommandEmpty>No language found.</CommandEmpty>
+              )}
+              <CommandGroup>
+                {isOpen &&
+                  formattedAddress.length > 0 &&
+                  formattedAddress.map((address, idx) => (
+                    <CommandItem
+                      value={address}
+                      key={idx}
+                      onSelect={() => {
+                        form.setValue("address", address);
+                        setInputValue(address);
+                        setIsOpen(false);
+                      }}
+                    >
+                      {address}
+                    </CommandItem>
+                  ))}
+              </CommandGroup>
+            </Command>
+
+            <FormDescription>
+              Please enter the address of the school. This will be autocompleted
+              by Google Maps.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        );
+      }}
     />
   );
 }
