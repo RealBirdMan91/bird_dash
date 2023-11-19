85b8240e2cb43840d67fb81c93af2ec263667103
Julian Vogel <birdman.coding.1991@gmail.com>
2023-11-19 17:58:29 +0100

    rewrote addressInput

--- components/school/AddressInput.tsx
+++ components/school/AddressInput.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import {
   FormDescription,
   FormField,
@@ -14,125 +14,50 @@ import {
   CommandInput,
   CommandItem,
 } from "@/components/ui/command";
-import { UseFormReturn, set } from "react-hook-form";
+import { UseFormReturn } from "react-hook-form";
 import { CreateEmployeeType } from "@/types/employeeSchema";
-import getGoogleMapsApiClient from "@/lib/googleApiClient";
+
+import { useAddress } from "@/hooks/useAddress";
 
 type Props = {
   form: UseFormReturn<CreateEmployeeType>;
 };
 
 export default function AddressInput({ form }: Props) {
-  const [formattedAddress, setFormattedAddress] = useState<string[]>([]);
-  const sessionTokenRef = useRef<string>();
-  const timeoutRef = useRef<NodeJS.Timeout>();
-
-  const onAddressChange = (address: string) => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-
-    timeoutRef.current = setTimeout(async () => {
-      try {
-        const google = await getGoogleMapsApiClient();
-        if (!sessionTokenRef.current) {
-          sessionTokenRef.current =
-            new google.maps.places.AutocompleteSessionToken() as string;
-        }
-        const { predictions } =
-          await new google.maps.places.AutocompleteService().getPlacePredictions(
-            {
-              input: address,
-              types: ["geocode"],
-              sessionToken: sessionTokenRef.current,
-              region: "eur",
-              //componentRestrictions: { country: "de" },
-            }
-          );
-
-        const service = new google.maps.places.PlacesService(
-          document.createElement("div")
-        );
-
-        return predictions.slice(0, 10).map((predictedAddress) => {
-          service.getDetails(
-            { placeId: predictedAddress.place_id },
-            (place, status) => {
-              if (
-                status === google.maps.places.PlacesServiceStatus.OK &&
-                place &&
-                place.address_components
-              ) {
-                // Überprüfen, ob die Adresse eine Straße (route) und eine Hausnummer (street_number) enthält
-                const hasStreet = place.address_components.some((component) =>
-                  component.types.includes("route")
-                );
-                const hasNumber = place.address_components.some((component) =>
-                  component.types.includes("street_number")
-                );
-
-                if (hasStreet && hasNumber && place.formatted_address) {
-                  // Überprüfen, ob die Adresse bereits in der Liste enthalten ist, bevor sie hinzugefügt wird
-                  setFormattedAddress((prev) => {
-                    if (
-                      place.formatted_address &&
-                      !prev.includes(place.formatted_address)
-                    ) {
-                      return [...prev, place.formatted_address];
-                    } else {
-                      return [...prev]; // Gibt den unveränderten Zustand zurück, wenn die Adresse bereits vorhanden ist
-                    }
-                  });
-                }
-              }
-            }
-          );
-        });
-      } catch (err) {
-        setFormattedAddress([]);
-      }
-    }, 150);
-  };
-
-  function onChangeHandler(val: string) {
-    setInputValue(val);
-    form.setValue("address", "");
-    setIsOpen(true);
-    if (val.length > 4) {
-      onAddressChange(val);
-    }
-  }
-
-  const [inputValue, setInputValue] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+  const { addresses } = useAddress(form.watch().address);
+
   return (
     <FormField
       control={form.control}
       name="address"
-      render={({ field }) => {
-        return (
-          <FormItem className="flex flex-col">
-            <FormLabel>Address</FormLabel>
+      render={({ field }) => (
+        <FormItem className="flex flex-col">
+          <FormLabel>Address</FormLabel>
 
-            <Command>
-              <CommandInput
-                placeholder="Search language..."
-                onValueChange={(val) => onChangeHandler(val)}
-                value={inputValue}
-              />
-              {isOpen && formattedAddress.length > 0 && (
-                <CommandEmpty>No Street found yet</CommandEmpty>
-              )}
+          <Command>
+            <CommandInput
+              placeholder="Search address"
+              value={field.value}
+              onValueChange={(value) => {
+                form.setValue("address", value);
+                setIsOpen(true);
+              }}
+              autoComplete="off"
+            />
+            {addresses && addresses?.length > 0 && (
+              <CommandEmpty>No address found.</CommandEmpty>
+            )}
+            {isOpen && (
               <CommandGroup>
-                {isOpen &&
-                  formattedAddress.length > 0 &&
-                  formattedAddress.map((address, idx) => (
+                {addresses &&
+                  addresses?.length > 0 &&
+                  addresses.map((address) => (
                     <CommandItem
                       value={address}
-                      key={idx}
+                      key={address}
                       onSelect={() => {
                         form.setValue("address", address);
-                        setInputValue(address);
                         setIsOpen(false);
                       }}
                     >
@@ -140,16 +65,15 @@ export default function AddressInput({ form }: Props) {
                     </CommandItem>
                   ))}
               </CommandGroup>
-            </Command>
+            )}
+          </Command>
 
-            <FormDescription>
-              Please enter the address of the school. This will be autocompleted
-              by Google Maps.
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        );
-      }}
+          <FormDescription>
+            Please insert a street this will get autocomplete by google maps
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
     />
   );
 }
---
+++ hooks/useAddress.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import getGoogleMapsApiClient from "@/lib/googleApiClient";
+import debounce from "lodash.debounce";
+
+async function getPlacePredictions(
+  g: typeof google,
+  address: string,
+  sessionToken: string
+) {
+  const { predictions } =
+    await new g.maps.places.AutocompleteService().getPlacePredictions({
+      input: address,
+      types: ["geocode"],
+      sessionToken: sessionToken,
+      region: "eur",
+      //componentRestrictions: { country: 'de' },
+    });
+
+  const service = new google.maps.places.PlacesService(
+    document.createElement("div")
+  );
+
+  let promises = predictions.map((predictedAddress) => {
+    return new Promise<string>((resolve, reject) => {
+      service.getDetails(
+        { placeId: predictedAddress.place_id },
+        (place, status) => {
+          if (
+            status === google.maps.places.PlacesServiceStatus.OK &&
+            place &&
+            place.address_components
+          ) {
+            const hasStreet = place.address_components.some((component) =>
+              component.types.includes("route")
+            );
+            const hasNumber = place.address_components.some((component) =>
+              component.types.includes("street_number")
+            );
+            if (hasStreet && hasNumber && place.formatted_address) {
+              resolve(place.formatted_address);
+            }
+          }
+        }
+      );
+    });
+  });
+
+  const formattedAddresses = await Promise.all(promises);
+  return formattedAddresses;
+}
+
+export function useAddress(val: string) {
+  // Create a ref for the session token
+  const sessionTokenRef = useRef<string>();
+  const [debouncedValue, setDebouncedValue] = useState(val);
+
+  // Debounce updating the value
+  useEffect(() => {
+    const debouncer = debounce((value) => {
+      setDebouncedValue(value);
+    }, 300);
+
+    debouncer(val);
+
+    return () => debouncer.cancel();
+  }, [val]);
+
+  // Initialize the session token if it's not already set
+  if (!sessionTokenRef.current) {
+    getGoogleMapsApiClient().then((google) => {
+      if (!sessionTokenRef.current) {
+        sessionTokenRef.current =
+          new google.maps.places.AutocompleteSessionToken() as string;
+      }
+    });
+  }
+
+  const {
+    data: addresses,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["address", debouncedValue],
+    queryFn: async () => {
+      const google = await getGoogleMapsApiClient();
+      return getPlacePredictions(
+        google,
+        debouncedValue,
+        sessionTokenRef.current as string
+      );
+    },
+    staleTime: 1000 * 60,
+    enabled: debouncedValue.length > 4,
+  });
+
+  return { addresses, isError, isLoading };
+}
